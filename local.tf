3b5b158e644b1dcb5aa447c1596ccda6196e54c2
Boris Yakimov <boris.yakimov@itgix.com>
2025-11-24 17:31:58 +0200

    #176 remove conformance packs from the module

--- local.tf
+++ local.tf
@@ -1,10 +1,9 @@
 locals {
-  module_prefix                 = "itgix-landing-zone"
-  recorder_name                 = "${local.module_prefix}-recorder"
-  delivery_name                 = "${local.module_prefix}-delivery"
-  aggregator_name               = "${local.module_prefix}-aggregator"
-  config_role_name              = "${local.module_prefix}-config-role"
-  aggregator_role_name          = "${local.module_prefix}-aggregator-role"
-  central_bucket_name           = "${local.module_prefix}-aws-config-history"
-  conformance_packs_bucket_name = "${local.module_prefix}-aws-config-conformance-packs"
+  module_prefix        = "itgix-landing-zone"
+  recorder_name        = "${local.module_prefix}-recorder"
+  delivery_name        = "${local.module_prefix}-delivery"
+  aggregator_name      = "${local.module_prefix}-aggregator"
+  config_role_name     = "${local.module_prefix}-config-role"
+  aggregator_role_name = "${local.module_prefix}-aggregator-role"
+  central_bucket_name  = "${local.module_prefix}-aws-config-history"
 }
