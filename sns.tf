7fb17265d0aed518310ff5cd444ccc7f2b483b0b
Boris Yakimov <boris.yakimov@itgix.com>
2026-01-14 14:08:11 +0200

    fix conditional to make sure sns topic is created only in the security account

--- sns.tf
+++ sns.tf
@@ -1,5 +1,5 @@
 resource "aws_sns_topic" "config_notifications" {
-  count = var.create_sns_topic && var.sns_topic_arn == null ? 1 : 0
+  count = var.is_security_account && var.create_sns_topic && var.sns_topic_arn == null ? 1 : 0
   name  = local.sns_topic_name
 
   tags = var.tags
