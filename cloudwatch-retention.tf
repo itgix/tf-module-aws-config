22b3296c739b6dcdbac7ee89ed28ff3bc48d06c7
Boris Yakimov <boris.yakimov@itgix.com>
2026-01-19 11:08:49 +0200

    #221 updates to the cloudwatch remediation approach

--- cloudwatch-retention.tf
+++ cloudwatch-retention.tf
@@ -1,6 +1,19 @@
+// Check if there are log groups that have never expire
+resource "aws_config_organization_managed_rule" "cw_log_retention_check" {
+  count           = var.is_security_account && var.cloudwatch_log_retention_remediation ? 1 : 0
+  name            = "cloudwatch-log-group-retention-check"
+  rule_identifier = "CLOUDWATCH_LOG_GROUP_RETENTION_PERIOD_CHECK"
+
+  description = "Ensure CloudWatch Log Groups do not use Never Expire retention"
+
+  input_parameters = jsonencode({
+    MinRetentionTime = 1
+  })
+}
+
 // Used to handle automatic remediation of log groups that do not have a retention set
 resource "aws_iam_role" "config_remediation_role" {
-  count = var.is_security_account && var.cloudwatch_log_retention_remediation ? 1 : 0
+  count = var.cloudwatch_log_retention_remediation ? 1 : 0
   name  = "AWSConfigRemediation-CloudWatchLogRetention"
 
   assume_role_policy = jsonencode({
@@ -18,7 +31,7 @@ resource "aws_iam_role" "config_remediation_role" {
 }
 
 resource "aws_iam_role_policy" "config_remediation_policy" {
-  count = var.is_security_account && var.cloudwatch_log_retention_remediation ? 1 : 0
+  count = var.cloudwatch_log_retention_remediation ? 1 : 0
   role  = aws_iam_role.config_remediation_role[0].id
 
   policy = jsonencode({
@@ -36,26 +49,9 @@ resource "aws_iam_role_policy" "config_remediation_policy" {
   })
 }
 
-// Check if there are log groups that have never expire
-resource "aws_config_organization_managed_rule" "cw_log_retention_check" {
-  count           = var.is_security_account && var.cloudwatch_log_retention_remediation ? 1 : 0
-  name            = "cloudwatch-log-group-retention-check"
-  rule_identifier = "CLOUDWATCH_LOG_GROUP_RETENTION_PERIOD_CHECK"
-
-  description = "Ensure CloudWatch Log Groups do not use Never Expire retention"
-
-  input_parameters = jsonencode({
-    MinRetentionTime = 1
-  })
-}
-
-// Automatically set expiration on any log group that is set to never expire
-// TODO: seems this kind of resource doesn't exist and we will have to do this in each account individually
-// maybe just add a conditional and call the same module in all accounts like we do with other modules
-resource "aws_config_organization_remediation_configuration" "cw_log_retention_remediation" {
-  count     = var.is_security_account && var.cloudwatch_log_retention_remediation ? 1 : 0
-  rule_name = aws_config_organization_managed_rule.cw_log_retention_check[0].name
-
+resource "aws_config_remediation_configuration" "cw_log_retention" {
+  count       = var.cloudwatch_log_retention_remediation ? 1 : 0
+  rule_name   = "cloudwatch-log-group-retention-check"
   target_type = "SSM_DOCUMENT"
   target_id   = "AWSConfigRemediation-SetCloudWatchLogGroupRetention"
 
@@ -70,7 +66,7 @@ resource "aws_config_organization_remediation_configuration" "cw_log_retention_r
 
   parameter {
     name         = "AutomationAssumeRole"
-    static_value = aws_iam_role.config_remediation_role[0].arn
+    static_value = "arn:aws:iam::${var.current_account_id}:role/AWSConfigRemediation-CloudWatchLogRetention"
   }
 
   resource_type = "AWS::Logs::LogGroup"
--- variables.tf
+++ variables.tf
@@ -10,6 +10,14 @@ variable "is_logging_account" {
   default     = false
 }
 
+variable "tags" {
+  type = map(string)
+  default = {
+    Name = "itgix-landing-zone"
+  }
+}
+
+# SNS
 variable "sns_topic_arn" {
   description = "Existing SNS topic ARN for Config delivery notifications. If not provided, a new SNS topic will be created."
   type        = string
@@ -22,21 +30,21 @@ variable "create_sns_topic" {
   default     = true
 }
 
+# S3
 variable "aws_config_central_bucket_name" {
   description = "Name of the S3 bucket that will store AWS Config aggregation history files (has to be a unique name globally in AWS)"
   type        = string
   default     = "itgix-landing-zone-aws-config-history"
 }
 
+# Cloudwatch log group retention and remediation
 variable "cloudwatch_log_retention_remediation" {
   description = "Enable org-wide CloudWatch Log Group retention enforcement and remediation"
   type        = bool
   default     = true
 }
 
-variable "tags" {
-  type = map(string)
-  default = {
-    Name = "itgix-landing-zone"
-  }
+variable "current_account_id" {
+  type        = string
+  description = "Account ID of the current account where the module is called from, used to configure auto-remediation of log groups in each account"
 }
